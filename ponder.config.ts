34d17430010862568179d72e08e0335f5a32743c
shrugs <mattgcondon@gmail.com>
2023-07-24 16:38:18 -0400

    fix: filter out irrelevant networks

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,15 +1,19 @@
 import type { Config } from "@ponder/core";
 import { Address } from "viem";
 
+const NETWORK = process.env.NETWORK ?? "anvil";
+
 const infuraUrl = (chain: string) =>
   `https://${chain}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+
+console.log(`Configured with network ${NETWORK}`);
 
 export const config: Config = {
   networks: [
     // { name: "mainnet", chainId: 1, rpcUrl: process.env.PONDER_RPC_URL_1 },
     { name: "goerli", chainId: 5, rpcUrl: infuraUrl("goerli") },
     { name: "anvil", chainId: 1, rpcUrl: "http://127.0.0.1:8545" },
-  ],
+  ].filter((network) => network.name === NETWORK),
   contracts: [
     {
       network: "anvil",
@@ -21,9 +25,9 @@ export const config: Config = {
     {
       network: "goerli",
       name: "Citi",
-      address: "0x91C259b9261593873d9705A2d85b6448e531BbB1" as Address,
+      address: "0xA7C2D39345824565ED8868B9625383951931c756" as Address,
       abi: "./abis/Citi.json",
       startBlock: 9383592,
     },
-  ].filter((config) => config.network === (process.env.NETWORK as string)),
+  ].filter((config) => config.network === NETWORK),
 };
